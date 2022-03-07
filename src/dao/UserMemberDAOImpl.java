0b4382b6fdb48642f5169e962212adae2bcf58b0
jonghwee <pjh52682428@gmail.com>
2022-03-07 17:03:53 +0900

    Revert "회원가입 기능 구현"
    This reverts commit 3b079705c9ba44cf0458b73d79b0a122bddf7a9e.

--- src/dao/UserMemberDAOImpl.java
+++ src/dao/UserMemberDAOImpl.java
@@ -36,7 +36,6 @@ public class UserMemberDAOImpl implements UserMemberDAO {
     @Override
     public int insertUser(UserMember userMember) throws SQLException {
     	String userId;
-    	int result=0;
     	
     	Connection con = null;
     	PreparedStatement ps = null;
@@ -44,33 +43,28 @@ public class UserMemberDAOImpl implements UserMemberDAO {
     	UserMember usermember = null;
 
     	try {
-	    	con = SampleUtils.getConnection();
+	    	con = DbUtil.getConnection();
 			ps = con.prepareStatement("insert into board (userid, userpw, username, userphone, userbirthday, userjoindate) values (?, ?, ?, ?, ?, sysdate)");
 			
-			ps.setString(1, usermember.getUserId());
+			ps.setString(1, usermember.);
 			rs = ps.executeQuery();
 			
-			ps.setString(2, usermember.getUserPw());
+			ps.setString(2, userPassword);
 			rs = ps.executeQuery();
 			
-			ps.setString(3, usermember.getUserName());
+			ps.setString(3, userName);
 			rs = ps.executeQuery();
 			
-			ps.setInt(4, usermember.getUserPhone());
+			ps.setInt(4, userPhone);
 			rs = ps.executeQuery();
 			
-			ps.setInt(5, usermember.getUserBirthDay());
+			ps.setInt(5, userBirthDay);
 			rs = ps.executeQuery();
 			
-			result = ps.executeUpdate();
-			System.out.println("result = " + result);
-			
-    	}catch (SQLException e) {
-    		e.printStackTrace();
     	}finally {
-    		SampleUtils.close(con, ps, rs);
+    		
     	}
-        return result;
+        return 0;
     }
 
     @Override
