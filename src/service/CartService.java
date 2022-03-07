be72f39bfa0a6d6cb83ca42e71971e22b8e936e3
David Oh <davidoh72@gmail.com>
2022-03-07 18:08:33 +0900

    CartController, CartService 추가

---
+++ src/service/CartService.java
@@ -0,0 +1,97 @@
+package service;
+
+import dto.CartProduct;
+import dto.UserMember;
+
+import java.util.List;
+import java.util.Map;
+
+public class CartService {
+    private ProductService productService = new ProductService();
+    private Map<String, List<CartProduct>> userCart; //String : User 객체의 userId
+    /* 일단 보류
+    private List<Product> allProducts
+    private List<ProductOption> allProductOptions
+     */
+
+    public CartService(){}
+
+    /////////////////////////////////////////////
+    ///////////////public methods///////////////
+    /*
+
+     */
+    public boolean insertUserSession(String userId){
+        //toDo
+
+        return false;
+    }
+
+    /*
+
+     */
+    public void handleProductOrder(){
+        //toDo
+    }
+
+    /*
+
+     */
+    public boolean increaseUserCartQuantity(int quantity){
+        //toDo
+
+        return false;
+    }
+
+    /*
+
+     */
+    public boolean decreaseUserCartQuantity(int quantity){
+        //toDo
+
+        return false;
+    }
+
+    /*
+
+     */
+    public Map<String,List<CartProduct>> getUserCart() {
+        //toDo
+
+        return null;
+    }
+
+    /*
+
+     */
+    public boolean clearUserCart() {
+        //toDo
+
+        return false;
+    }
+
+    /////////////////////////////////////////////
+    ///////////////private methods///////////////
+    private void viewCartProducts() {
+        //toDo
+    }
+
+    private void insertProductToUserCart() {
+        //toDo
+    }
+
+    private void insertOptionToUserCart() {
+        //toDO
+    }
+
+    /* 일단 보류
+    private List<Product> getAllProducts(){}
+    private List<ProductOption> getAllProductOptions(){}
+    private List<Product> getProductsByCategory(String category){}
+    private List<ProductOption> getProductOptionsByCategory(String category){}
+     */
+
+
+
+
+}
