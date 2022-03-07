9e98f783929731dbf214cc9afeaa227bac4a792a
David Oh <davidoh72@gmail.com>
2022-03-07 12:27:53 +0900

    Oracle Cloud DB 접속 테스트

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,11 @@
+import utils.SampleUtils;
+
+import java.sql.SQLException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Hello world");
+        SampleUtils.getConnection();
+        System.out.println("made it here");
     }
 }
--- src/utils/SampleUtils.java
+++ src/utils/SampleUtils.java
@@ -1,4 +1,48 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.sql.*;
+import java.util.Properties;
+
 public class SampleUtils {
+    private static Properties proFile = new Properties();
+
+
+
+    static {
+        try {
+            proFile.load(new FileInputStream("src/resources/dbInfo.properties"));
+            //proFile.load(new FileInputStream("board.properties"));
+
+            Class.forName(proFile.getProperty("driverName"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Properties getProFile() {
+        return proFile;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                proFile.getProperty("url"),
+                proFile.getProperty("userName"),
+                proFile.getProperty("userPass"));
+    }
+
+
+    public static void close(Connection con, Statement st, ResultSet rs) {
+        try {
+            if(rs != null) rs.close();
+            if(st != null) st.close();
+            if(con != null) con.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
