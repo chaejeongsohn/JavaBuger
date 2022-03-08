45c98c6c786d072b02a249d48456885f1ae01c40
soobin <a01056239778@gmail.com>
2022-03-08 23:21:06 +0900

    #45

---
+++ src/view/menu/ManagerDetailMenuView.java
@@ -0,0 +1,98 @@
+package view.menu;
+
+import controller.CouponController;
+import dto.Coupon;
+import service.UserSessionService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ManagerDetailMenuView {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void printCouponMenu(){
+        System.out.println("-----------------------------------");
+        System.out.println("1.쿠폰추가  |  2.쿠폰삭제  |  3.쿠폰수정  |  4.쿠폰목록  |  5.이전으로 돌아가기");
+        System.out.println("메뉴를 선택하세요 > ");
+        int menu = Integer.parseInt(scanner.nextLine());
+        switch (menu) {
+            case 1: // 쿠폰추가
+                printInsertCouponMenu();
+                break;
+            case 2: // 쿠폰삭제
+                printDeleteCouponMenu();
+                break;
+            case 3: // 쿠폰수정
+                printUpdateCouponMenu();
+                break;
+            case 4: // 쿠폰 목록
+                printAllCoupons();
+                break;
+            case 5:  // 이전으로 돌아가기
+                return;
+        }
+
+
+    }
+
+    private static void printInsertCouponMenu(){
+        System.out.println("--------- 추가할 쿠폰 정보 ----------");
+        System.out.print("쿠폰 번호 입력 > ");
+        int couponNumber = Integer.parseInt(scanner.nextLine());
+        System.out.print("쿠폰 내용 입력 > ");
+        String couponDetail = scanner.nextLine();
+        System.out.print("쿠폰 할인률 입력 (10%일 경우 10입력) > ");
+        int couponDiscountRate = Integer.parseInt(scanner.nextLine());
+        System.out.print("쿠폰 만료기한 입력 (숫자만) > ");
+        int couponExpiration = Integer.parseInt(scanner.nextLine());
+        System.out.println("------------------------------------");
+        Coupon coupon = new Coupon(couponNumber, couponDetail,couponDiscountRate, couponExpiration);
+        CouponController.insertCoupon(coupon);
+    }
+
+    private static void printDeleteCouponMenu(){
+        printAllCoupons();
+        System.out.print("삭제할 쿠폰번호를 입력하세요 > ");
+        int couponNumber = Integer.parseInt(scanner.nextLine());
+        CouponController.deleteCoupon(couponNumber);
+    }
+
+    private static void printUpdateCouponMenu(){
+        printAllCoupons(); // 쿠폰 종류 전체 출력
+        System.out.print("수정할 쿠폰번호를 입력하세요 > ");
+        int couponNumber = Integer.parseInt(scanner.nextLine());
+        Coupon coupon = CouponController.selectCouponByNumber(couponNumber);
+
+        System.out.println("---------쿠폰 수정 메뉴 -----------");
+        System.out.println("1.쿠폰 내용 변경 2.쿠폰 할인률 변경 3.쿠폰 만료기한 변경 4. 이전으로 돌아가기");
+        System.out.println("메뉴를 선택하세요 > ");
+        int menu = Integer.parseInt(scanner.nextLine());
+        switch (menu) {
+            case 1: // 쿠폰내용변경
+                System.out.print("새로운 쿠폰내용은? > ");
+                String couponDetail = scanner.nextLine();
+                coupon.setCouponDetail(couponDetail);
+                break;
+            case 2: // 폰번호변경
+                System.out.print("새로운 쿠폰 할인률은? > ");
+                int couponDiscountRate = Integer.parseInt(scanner.nextLine());
+                coupon.setCouponDiscountRate(couponDiscountRate);
+
+                break;
+            case 3: // 쿠폰만료기한
+                System.out.print("새로운 쿠폰 만료기한은? > ");
+                int couponExpiration = Integer.parseInt(scanner.nextLine());
+                coupon.setCouponExpiration(couponExpiration);
+                break;
+            case 4: // 이전으로 돌아가기
+                return;
+        }
+        CouponController.updateCoupon(coupon);
+    }
+
+    private static void printAllCoupons(){
+        System.out.println("--------쿠폰 목록-----------");
+        CouponController.selectCoupons();
+    }
+}
--- src/view/menu/ManagerMenuView.java
+++ src/view/menu/ManagerMenuView.java
@@ -1,225 +1,227 @@
 package view.menu;
 
-import java.util.Scanner;
-
 import controller.ProductController;
 import controller.ProductOptionController;
 import dto.Product;
 import dto.ProductOption;
-import service.ProductService;
+
+import java.util.Scanner;
 
 public class ManagerMenuView {
-	private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
-	public static void managerMenu() {
-		while (true) {
-			printManagerMenu();
-			System.out.println("메뉴를 선택해주세요 > ");
-			int menu = Integer.parseInt(scanner.nextLine());
-			switch (menu) {
-			case 1: // 상품관리
-				productControl();
-				break;
-			case 2: // 옵션관리
-				optionControl();
-				break;
-			case 3: // 매출관리
-				break;
-			case 4: // 매점관리
-				break;
-			case 5: // 프로그램 종료
-				System.exit(0);
-			}
-		}
-	}
+    public static void managerMenu() {
+        while (true) {
+            printManagerMenu();
+            System.out.println("메뉴를 선택해주세요 > ");
+            int menu = Integer.parseInt(scanner.nextLine());
+            switch (menu) {
+                case 1: // 상품관리
+                    productControl();
+                    break;
+                case 2: // 옵션관리
+                    optionControl();
+                    break;
+                case 3: // 매출관리
+                    break;
+                case 4: // 매점관리
+                    break;
+                case 5:
+                    ManagerDetailMenuView.printCouponMenu();
+                    break;
+                case 6: // 프로그램 종료
+                    System.exit(0);
+            }
+        }
+    }
 
-	private static void printManagerMenu() {
-		System.out.println("---------------------");
-		System.out.println("1. 상품관리  2. 옵션관리  3. 매출관리  4. 매점관리  5. 프로그램종료");
-	}
+    private static void printManagerMenu() {
+        System.out.println("---------------------");
+        System.out.println("1. 상품관리  2. 옵션관리  3. 매출관리  4. 매점관리  5.쿠폰관리  6. 프로그램종료");
+    }
 
-	private static void productControl() {
-		System.out.println("원하는 상품관리를 선택해주세요 >");
-		System.out.println("1. 상품수정  2. 상품등록  3. 상품삭제  4. 품절등록");
-		while (true) {
-			int menu = Integer.parseInt(scanner.nextLine());
-			switch (menu) {
-			case 1: // 상품수정
-				printUpdateProductMenu();
+    private static void productControl() {
+        System.out.println("원하는 상품관리를 선택해주세요 >");
+        System.out.println("1. 상품수정  2. 상품등록  3. 상품삭제  4. 품절등록");
+        while (true) {
+            int menu = Integer.parseInt(scanner.nextLine());
+            switch (menu) {
+                case 1: // 상품수정
+                    printUpdateProductMenu();
 
-				break;
-			case 2: // 상품등록
+                    break;
+                case 2: // 상품등록
 
-				printInsertProductMenu();
-				break;
-			case 3: // 상품삭제
-				printDeleteProductMenu();
-				break;
-			case 4: // 품절등록
-				System.out.println("품절등록할 상품번호를 입력해주세요. >");
-				break;
+                    printInsertProductMenu();
+                    break;
+                case 3: // 상품삭제
+                    printDeleteProductMenu();
+                    break;
+                case 4: // 품절등록
+                    System.out.println("품절등록할 상품번호를 입력해주세요. >");
+                    break;
 
-			}
-		}
+            }
+        }
 
-	}// productControl 메소드 끝
+    }// productControl 메소드 끝
 
-	private static void printUpdateProductMenu() {
+    private static void printUpdateProductMenu() {
 
-		ProductController.selectProducts();
+        ProductController.selectProducts();
 
-		System.out.println("수정할 상품 번호를 입력해주세요. >");
-		int productNumber = Integer.parseInt(scanner.nextLine());
-		Product selectProduct = ProductController.selectProductByProductNumber(productNumber);
+        System.out.println("수정할 상품 번호를 입력해주세요. >");
+        int productNumber = Integer.parseInt(scanner.nextLine());
+        Product selectProduct = ProductController.selectProductByProductNumber(productNumber);
 
-		while (true) {
-			System.out.println("1. 상품이름 변경   2. 상품가격 변경  3. 상품설명 변경 ");
-			System.out.println("메뉴 선택해주세요 > ");
-			int menu = Integer.parseInt(scanner.nextLine());
-			switch (menu) {
-			case 1: // 상품이름 수정
-				System.out.println("수정할 상품 이름를 입력해주세요. >");
-				String name = scanner.nextLine();
-				selectProduct.setProductName(name);
-				ProductController.updateProduct(selectProduct);
-				break;
+        while (true) {
+            System.out.println("1. 상품이름 변경   2. 상품가격 변경  3. 상품설명 변경 ");
+            System.out.println("메뉴 선택해주세요 > ");
+            int menu = Integer.parseInt(scanner.nextLine());
+            switch (menu) {
+                case 1: // 상품이름 수정
+                    System.out.println("수정할 상품 이름를 입력해주세요. >");
+                    String name = scanner.nextLine();
+                    selectProduct.setProductName(name);
+                    ProductController.updateProduct(selectProduct);
+                    break;
 
-			case 2: // 상품가격 변경
-				System.out.println("수정할 상품 가격를 입력해주세요. >");
-				int price = scanner.nextInt();
-				selectProduct.setProductNumber(price);
-				ProductController.updateProduct(selectProduct);
-				break;
+                case 2: // 상품가격 변경
+                    System.out.println("수정할 상품 가격를 입력해주세요. >");
+                    int price = scanner.nextInt();
+                    selectProduct.setProductNumber(price);
+                    ProductController.updateProduct(selectProduct);
+                    break;
 
-			case 3: // 상품설명 변경
-				System.out.println("수정할 상품 설명을 입력해주세요. >");
-				String detail = scanner.nextLine();
-				selectProduct.setProductDetail(detail);
-				ProductController.updateProduct(selectProduct);
-				break;
+                case 3: // 상품설명 변경
+                    System.out.println("수정할 상품 설명을 입력해주세요. >");
+                    String detail = scanner.nextLine();
+                    selectProduct.setProductDetail(detail);
+                    ProductController.updateProduct(selectProduct);
+                    break;
 
-			}// switch
+            }// switch
 
-		} // while
+        } // while
 
-	}// printUpdateProductMenu 메소드
+    }// printUpdateProductMenu 메소드
 
-	private static void printInsertProductMenu() {
-		ProductController.selectProducts();
+    private static void printInsertProductMenu() {
+        ProductController.selectProducts();
 
-		System.out.print("등록할 상품번호는 ? ");
-		int productNumber = Integer.parseInt(scanner.nextLine());
+        System.out.print("등록할 상품번호는 ? ");
+        int productNumber = Integer.parseInt(scanner.nextLine());
 
-		System.out.print("등록할 상품카테고리번호는 ? ");
-		String categoryNumber = scanner.nextLine();
+        System.out.print("등록할 상품카테고리번호는 ? ");
+        String categoryNumber = scanner.nextLine();
 
-		System.out.print("등록할 상품이름은 ? ");
-		String productName = scanner.nextLine();
+        System.out.print("등록할 상품이름은 ? ");
+        String productName = scanner.nextLine();
 
-		System.out.print("등록할 상품가격은 ? ");
-		int productPrice = Integer.parseInt(scanner.nextLine());
+        System.out.print("등록할 상품가격은 ? ");
+        int productPrice = Integer.parseInt(scanner.nextLine());
 
-		System.out.print("등록할 상품설명은 ? ");
-		String productDetail = scanner.nextLine();
+        System.out.print("등록할 상품설명은 ? ");
+        String productDetail = scanner.nextLine();
 
-		ProductController
-				.insertProduct(new Product(productNumber, categoryNumber, productName, productPrice, productDetail));
+        ProductController
+                .insertProduct(new Product(productNumber, categoryNumber, productName, productPrice, productDetail));
 
-	}// printInsertProductMenu 메소드 끝
+    }// printInsertProductMenu 메소드 끝
 
-	private static void printDeleteProductMenu() {
-		ProductController.selectProducts();
+    private static void printDeleteProductMenu() {
+        ProductController.selectProducts();
 
-		System.out.println("삭제할 상품번호을 선택해주세요. >");
-		int productNumber = Integer.parseInt(scanner.nextLine());
-		Product selectProduct = ProductController.selectProductByProductNumber(productNumber);
+        System.out.println("삭제할 상품번호을 선택해주세요. >");
+        int productNumber = Integer.parseInt(scanner.nextLine());
+        Product selectProduct = ProductController.selectProductByProductNumber(productNumber);
 
-		if (selectProduct != null) {
-			ProductController.deleteProduct(productNumber);
-		}
+        if (selectProduct != null) {
+            ProductController.deleteProduct(productNumber);
+        }
 
-	}// printDeleteProductMenu 메소드
+    }// printDeleteProductMenu 메소드
 
-	private static void optionControl() {
-		System.out.println("원하는 옵션관리를 선택해주세요 >");
-		System.out.println("1. 옵션수정  2. 옵션등록  3. 옵션삭제 ");
-		while (true) {
-			int menu = Integer.parseInt(scanner.nextLine());
-			switch (menu) {
-			case 1: // 옵션수정
-				printUpdateOptionMenu();
-				break;
-			case 2: // 옵션등록
-				printInsertOptionMenu();
-				break;
-			case 3: // 옵션삭제
-				printDeleteOptionMenu();
-				break;
+    private static void optionControl() {
+        System.out.println("원하는 옵션관리를 선택해주세요 >");
+        System.out.println("1. 옵션수정  2. 옵션등록  3. 옵션삭제 ");
+        while (true) {
+            int menu = Integer.parseInt(scanner.nextLine());
+            switch (menu) {
+                case 1: // 옵션수정
+                    printUpdateOptionMenu();
+                    break;
+                case 2: // 옵션등록
+                    printInsertOptionMenu();
+                    break;
+                case 3: // 옵션삭제
+                    printDeleteOptionMenu();
+                    break;
 
-			}
-		}
+            }
+        }
 
-	}// optionControl 메소드 끝
+    }// optionControl 메소드 끝
 
-	private static void printUpdateOptionMenu() {
-		ProductOptionController.selectProductOptions();
-		System.out.println("수정할 옵션 번호를 입력해주세요. >");
-		int optionNumber = Integer.parseInt(scanner.nextLine());
-		ProductOption selectOption = ProductOptionController.selectProductOptionByOptionNumber(optionNumber);
+    private static void printUpdateOptionMenu() {
+        ProductOptionController.selectProductOptions();
+        System.out.println("수정할 옵션 번호를 입력해주세요. >");
+        int optionNumber = Integer.parseInt(scanner.nextLine());
+        ProductOption selectOption = ProductOptionController.selectProductOptionByOptionNumber(optionNumber);
 
-		while (true) {
-			System.out.println("1. 옵션이름 변경   2. 옵션가격 변경  ");
-			System.out.println("메뉴 선택해주세요 > ");
-			int menu = Integer.parseInt(scanner.nextLine());
-			switch (menu) {
-			case 1: // 옵션이름 수정
-				System.out.println("수정할 옵션 이름를 입력해주세요. >");
-				String name = scanner.nextLine();
-				selectOption.setOptionName(name);
-				ProductOptionController.updateProductOption(selectOption);
-				break;
+        while (true) {
+            System.out.println("1. 옵션이름 변경   2. 옵션가격 변경  ");
+            System.out.println("메뉴 선택해주세요 > ");
+            int menu = Integer.parseInt(scanner.nextLine());
+            switch (menu) {
+                case 1: // 옵션이름 수정
+                    System.out.println("수정할 옵션 이름를 입력해주세요. >");
+                    String name = scanner.nextLine();
+                    selectOption.setOptionName(name);
+                    ProductOptionController.updateProductOption(selectOption);
+                    break;
 
-			case 2: // 옵션가격 변경
-				System.out.println("수정할 옵션 가격를 입력해주세요. >");
-				int price = scanner.nextInt();
-				selectOption.setOptionPrice(price);
-				ProductOptionController.updateProductOption(selectOption);
-				break;
+                case 2: // 옵션가격 변경
+                    System.out.println("수정할 옵션 가격를 입력해주세요. >");
+                    int price = scanner.nextInt();
+                    selectOption.setOptionPrice(price);
+                    ProductOptionController.updateProductOption(selectOption);
+                    break;
 
-			}// switch
+            }// switch
 
-		} // while*/
-	}
+        } // while*/
+    }
 
-	private static void printInsertOptionMenu() {
-		ProductOptionController.selectProductOptions();
-		
-		System.out.print("등록할 옵션번호는 ? ");
-		int optionNumber = Integer.parseInt(scanner.nextLine());
+    private static void printInsertOptionMenu() {
+        ProductOptionController.selectProductOptions();
 
-		System.out.print("등록할 옵션카테고리번호는 ? ");
-		String categoryNumber = scanner.nextLine();
+        System.out.print("등록할 옵션번호는 ? ");
+        int optionNumber = Integer.parseInt(scanner.nextLine());
 
-		System.out.print("등록할 옵션이름은 ? ");
-		String optionName = scanner.nextLine();
+        System.out.print("등록할 옵션카테고리번호는 ? ");
+        String categoryNumber = scanner.nextLine();
 
-		System.out.print("등록할 옵션가격은 ? ");
-		int optionPrice = Integer.parseInt(scanner.nextLine());
+        System.out.print("등록할 옵션이름은 ? ");
+        String optionName = scanner.nextLine();
 
-		ProductOptionController
-				.insertProductOption(new ProductOption(optionNumber, categoryNumber, optionName, optionPrice));
-	}
+        System.out.print("등록할 옵션가격은 ? ");
+        int optionPrice = Integer.parseInt(scanner.nextLine());
 
-	private static void printDeleteOptionMenu() {
-		ProductOptionController.selectProductOptions();
+        ProductOptionController
+                .insertProductOption(new ProductOption(optionNumber, categoryNumber, optionName, optionPrice));
+    }
 
-		System.out.println("삭제할 옵션번호을 선택해주세요. >");
-		int optionNumber = Integer.parseInt(scanner.nextLine());
-		ProductOption selectOption = ProductOptionController.selectProductOptionByOptionNumber(optionNumber);
+    private static void printDeleteOptionMenu() {
+        ProductOptionController.selectProductOptions();
 
-		if (selectOption != null) {
-			ProductOptionController.deleteProductOption(optionNumber);
-		}
-	}
+        System.out.println("삭제할 옵션번호을 선택해주세요. >");
+        int optionNumber = Integer.parseInt(scanner.nextLine());
+        ProductOption selectOption = ProductOptionController.selectProductOptionByOptionNumber(optionNumber);
+
+        if (selectOption != null) {
+            ProductOptionController.deleteProductOption(optionNumber);
+        }
+    }
 
 }
