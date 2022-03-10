d82e511d94834e4c145f2dd1d64337fe2820744a
soobin <a01056239778@gmail.com>
2022-03-10 15:11:22 +0900

    #82 비회원 삭제

--- src/view/menu/RoleMenuView.java
+++ src/view/menu/RoleMenuView.java
@@ -16,13 +16,10 @@ public class RoleMenuView {
                 case "1": // 회원
                     UserMenuView.userMenu();
                     break;
-                case "2": // 비회원
-                    OrderMenuView.orderMenu();
-                    break;
-                case "3": // 매니저
+                case "2": // 매니저
                     ManagerMenuView.managerMenu();
                     break;
-                case "4": // 프로그램종료
+                case "3": // 프로그램종료
                 	System.out.println("프로그램을 종료합니다.");
                     System.exit(0);
                 default:
@@ -33,7 +30,7 @@ public class RoleMenuView {
 
     private static void printMenu(){
         System.out.println("---- ★JAVA BURGER★에 오신 것을 환영합니다 ^^ ----");
-        System.out.println("1. 회원   |   2. 비회원   |   3. 매니저   |   4. 프로그램 종료");
+        System.out.println("1. 회원   |   2. 매니저   |   3. 프로그램 종료");
 
     }
 }
